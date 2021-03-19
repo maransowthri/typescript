fb7853f61733eaefa37037a3a3e07270f3ca3541
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-03-19 09:02:00 +0530

    Finished advanced types

---
+++ advancedTypes/src/app.ts
@@ -0,0 +1,11 @@
+// console.log("Hello World!");
+
+// const paraEl = document.querySelector("p")!;
+
+const user = {
+  name: "Maran",
+  age: 22,
+  job: { designation: "SSE", company: "athena" },
+};
+
+console.log(user?.job?.company);
